962a6ffbf138a56f2465098ba991c58802d5bc8f
Sami Makhuli <roger.makhuli@gmail.com>
2025-05-22 11:58:37 -0400

    Updated run_callbacks method again [no ci]

--- core/app/workers/sidekiq/callbacks.rb
+++ core/app/workers/sidekiq/callbacks.rb
@@ -236,7 +236,7 @@ module Sidekiq
       end
     end
 
-    def run_callbacks(kind, with_children = true, *)
+    def run_callbacks(kind, with_children: true, &block)
       result = super
       _enqueue_callback_workers(kind) if result != false && kind != :find
       result
